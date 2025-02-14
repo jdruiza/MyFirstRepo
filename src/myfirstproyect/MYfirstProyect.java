886661103b92bd548ab680cf805df502a16fcb70
jjrui <jjrui@10.13.57.193>
2025-02-14 12:14:13 -0600

    Se actualizo otra linea de prueba 14 feb 2025

--- src/myfirstproyect/MYfirstProyect.java
+++ src/myfirstproyect/MYfirstProyect.java
@@ -16,6 +16,8 @@ public class MYfirstProyect {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Procesando una Linea");
+
     }
     
 }
